21865b4a6d87eabb57b828a90e45d7df18a9155d
Subhranil Dey <subhranildey@users.noreply.github.com>
2016-08-02 00:40:36 +0530

    Update AD-integration1.sh

--- AD-integration1.sh
+++ AD-integration1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+
 cd ~
 wget 'http://download.beyondtrust.com/PBISO/8.0.0.2016/linux.deb.x64/pbis-open-8.0.0.2016.linux.x86_64.deb.sh'
 chmod +x pbis-open-8.0.0.2016.linux.x86_64.deb.sh
