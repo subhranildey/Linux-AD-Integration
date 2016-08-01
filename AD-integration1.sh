13f67d0cb147628ab0061665c6edbdb875fe1a65
Subhranil Dey <subhranildey@users.noreply.github.com>
2016-08-01 22:54:11 +0530

    Update AD-integration1.sh

--- AD-integration1.sh
+++ AD-integration1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd ~
 wget 'http://download.beyondtrust.com/PBISO/8.0.0.2016/linux.deb.x64/pbis-open-8.0.0.2016.linux.x86_64.deb.sh'
 chmod +x pbis-open-8.0.0.2016.linux.x86_64.deb.sh
